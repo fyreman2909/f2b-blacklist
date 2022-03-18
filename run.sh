a01e1e8bd894bcc6cefa0d92fc23db04c7671752
fyreman <46133450+fyreman2909@users.noreply.github.com>
2022-03-18 10:08:27 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-/sbin/ipset save f2b-sshd -f f2b-sshd.blacklist
+# Save own ipset from f2b-sshd
+/sbin/ipset save f2b-sshd -f f2b-sshd-$(hostname).blacklist
 
+# Push own file to git
 #git push
 
-#/sbin/ipset restore -f -! f2b-sshd-all.blacklist
+# Combine all ipset files
+cat f2b-sshd-*.blacklist > f2b-sshd_temp.blacklist
+
+# Sort and uniq only
+sort f2b-sshd_temp.blacklist | uniq > f2b-sshd_combined.blacklist
+
+# rm temp file
+rm -rf f2b-sshd_temp.blacklist
+
+# Restore combined f2b ipsets
+#/sbin/ipset restore -f -! f2b-sshd_combined.blacklist
